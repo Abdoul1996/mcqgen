24053cae95f1dd8f273270b84d1b40c346c68711
aabdillahi@mail.sfsu.edu <aabdillahi@mail.sfsu.edu>
2024-05-26 17:06:28 -0700

    file updated

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 # It used to install a local package in my virtual envirnonment 
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Abdoulfatah Abdillahi',
+    author_email='aabdillahid@gmail.com',
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages()
+)
